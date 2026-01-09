e6aad0500b05c7e79678526a941d978ad73fd17e
nirosha-expleo <nirosha.srinivasan@expleogroup.com>
2026-01-09 12:22:21 +0530

    Deploy VerifyUser to GitHub Pages - 2026-01-09

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2625416396"
+    serviceWorkerVersion: "3860381841"
   }
 });
